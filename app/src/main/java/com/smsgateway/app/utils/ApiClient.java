971b90c8083b722947121bd6842fe16d391ebbcd
itechnooffit <itechnooffit@gmail.com>
2026-06-06 22:19:16 +0300

    Update ApiClient.java

--- app/src/main/java/com/smsgateway/app/utils/ApiClient.java
+++ app/src/main/java/com/smsgateway/app/utils/ApiClient.java
@@ -103,7 +103,7 @@ public class ApiClient {
         new Thread(() -> {
             try {
                 RequestBody body = new FormBody.Builder()
-                        .add("deviceId", "261")
+                        .add("deviceId", "1")
                         .build();
                 Request req = buildRequest("/services/get-messages.php", body);
                 try (Response r = client.newCall(req).execute()) {
